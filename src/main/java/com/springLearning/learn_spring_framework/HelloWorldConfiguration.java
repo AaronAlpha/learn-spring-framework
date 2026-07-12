fbe7f6a8d38ff56ec655dc37ede381ec1f83327a
Aaron Mac 2026 <reachaaronbernard@gmail.com>
2026-07-11 23:45:43 -0600

    finished Step 10

--- src/main/java/com/springLearning/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/springLearning/learn_spring_framework/HelloWorldConfiguration.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.Configuration; // used to indicate
 
 
 // seeing if Spring can manage the object of a custom class
-record Person(String name, int age) { /* can define custom methods in these braces */ }; // defining 2 member vars (name and age)
+// record Person(String name, int age) { /* can define custom methods in these braces */ }; // defining 2 member vars (name and age)
 // "record" is after JDK16 release
 
 // typically, when creating a Java class, it would require creation a bunch of stuff like: public accessor methods, constructors, equals, hashcode and toString etc
 // however, the "record" keyword  eliminates verbosity in creating Java Beans, such as eliminating the need to create getter/setters or other "requirements" of a class
+
+
+record Person(String name, int age, Address address) { /* can define custom methods in these braces */ }; // defining 2 member vars (name and age)
+// making a new 'version' of Person (with an "Address" param in the constructor call
 
 
 // another custom class --> Address - firstLine & city
@@ -20,7 +24,7 @@ record Address(String firstLine, String city) { /* can define custom methods in 
 
 
 @Configuration // used to indicate a "@Configuration" class (Config-Class)
-public class HelloWorldConfiguration {
+public class  HelloWorldConfiguration {
 
     // define Spring Beans in here
     // the "things" managed by Spring Framework, known as "Spring Beans"
@@ -51,7 +55,7 @@ public class HelloWorldConfiguration {
     // all the "verbosity" is automatically created --> for eg: in the above record def, a "Person" constructor is automatically created with the 2 args
     @Bean
     public Person person(){
-        var person = new Person("Spring Bean - Obj Eg (Custom Class)", 301);
+        var person = new Person("Spring Bean - Obj Eg (Custom Class)", 301, new Address("hardcoded Addr val", "Note: would need to create a new obj, becoz the param is of an OBJECT of type Address, so would need to create a new \"Address\" obj") );
         // Note: becoz we use a "record", all the extra verbosity (such as getter, setter, constructor, toString, etc) are created automatically
         // thus, doing "person.age()", "person.name()" is viable. Additionally, the constructor is automatically run in the above line.
 
@@ -79,6 +83,48 @@ public class HelloWorldConfiguration {
         return address;
 
     }// address-bean
+
+
+
+
+    // creating a new "Person" - bean "person2" - using "method calls"
+    @Bean
+    public Person person2_MethodCall() {
+        // using existing Beans (managed by Spring) - Bean "name" and "age"
+
+        // using existing Bean-methods thru "method calls" - therefore, just "name()" and "age()" and "address()"
+        var person = new Person(this.name(), this.age(), this.address());
+
+
+        return person;
+    }
+
+    @Bean(name="addr2")
+    // Note: if the "@Bean" annotator is not included for a method def, upon doing "variable.getBean(<string val>)" to call the new method
+    // the output will be errored: "Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named <string val> available"
+    // becoz we calling .getBean(...), expecting to find a bean, but we have no methods that have the @Bean annotator
+    public Address address2(){
+        var address = new Address("Vancouver", "Berne");
+
+        return address;
+
+    }// address-bean
+
+    // creating a new "Person" - bean "person3" - by "passing params"
+    @Bean
+    public Person person3_Params(String name, int age, Address addr2) { // passing params, like "name", "address"
+        // Note!, we dont pass in "address" (which would be the name of the class-method, as seen above), but we instead pass-in the name of the bean,
+        // which we altered for the "address" class-method to be "addr"
+
+        // thus, in this case (unlike the person2), we create passable params (name, age and addr) which refer to existing Beans in this .java file,
+        // ensuring that the created params refer to the **name of the BEAN** not **name of the CLASS-METHOD**; if no Bean-name (param) is
+        // specified for a class-method, then the name of the method is the name of the Bean
+
+        var person = new Person(name, age, addr2);
+
+
+        return person;
+    }
 
 
 
